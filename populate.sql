f1177a6a8cdea2b4af2ad0267d2cc03070f0a5ed
HenriTEL <henri.hannetel@gmail.com>
2015-10-20 22:32:03 +0000

    vue chomage par année

--- populate.sql
+++ populate.sql
@@ -1,5 +1,9 @@
 CREATE DATABASE fact_checking CHARACTER SET utf8 COLLATE utf8_general_ci;
 CONNECT fact_checking;
+
+/*******************************************************************************
+**********     CHOMAGE
+*******************************************************************************/
 CREATE TABLE IF NOT EXISTS chomage (
 	ZE2010 INT NOT NULL,
 	LIBZE2010 VARCHAR(255) NOT NULL,
@@ -37,13 +41,25 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
+CREATE VIEW Vchomage AS SELECT
+SUM(T1_2008 + T2_2008 + T3_2008 + T4_2008) A2008,
+SUM(T1_2009 + T2_2009 + T3_2009 + T4_2009) A2009,
+SUM(T1_2010 + T2_2010 + T3_2010 + T4_2010) A2010,
+SUM(T1_2011 + T2_2011 + T3_2011 + T4_2011) A2011
+FROM chomage;
+
+/*******************************************************************************
+**********     POPULATION
+*******************************************************************************/
 CREATE TABLE IF NOT EXISTS population (
 	annee INT NOT NULL,
 	population_active INT NOT NULL
 );
+
 LOAD DATA INFILE '/home/hh/prog/FactChecking/population-active.csv' 
 INTO TABLE population
 FIELDS TERMINATED BY ';' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
+
