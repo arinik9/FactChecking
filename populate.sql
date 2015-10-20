5409d4837d740d8ec3fc8dd625ee1cf9926950c4
arinik9 <arinik9@hotmail.com>
2015-10-20 22:33:07 +0200

    adding 1 new view for 'chomage' table

--- populate.sql
+++ populate.sql
@@ -36,3 +36,28 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
+
+CREATE TABLE IF NOT EXISTS population (
+	annee INT NOT NULL,
+	population_active INT NOT NULL
+);
+LOAD DATA INFILE '/home/hh/prog/FactChecking/population-active.csv' 
+INTO TABLE population
+FIELDS TERMINATED BY ';' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+-- Pour créer un vue afin de l'utiliser l'operation 'join' sur la table chomage et population
+
+CREATE VIEW chomageByYear AS
+SELECT ROUND(ZE2010/100) as "regioncode", LIBZE2010 as "ville", sum(T2_2007+T3_2007+T4_2007)/3 as "2007",
+ sum(T1_2008+T2_2008+T3_2008+T4_2008)/4 as "2008",
+sum(T1_2009+T2_2009+T3_2009+T4_2009)/4 as "2009",
+sum(T1_2010+T2_2010+T3_2010+T4_2010)/4 as "2010",
+sum(T1_2011+T2_2011+T3_2011+T4_2011)/4 as "2011",
+T1_2012 as "2012"
+from chomage
+group by ZE2010;
+
+Select * from chomageByYear; 
